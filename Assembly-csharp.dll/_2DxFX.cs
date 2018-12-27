769096698f0a4ee1e611541cf420fe9c4e27b789
N204 <46192688+N204@users.noreply.github.com>
2018-12-27 13:49:23 +0100

    Create _2DxFX.cs

---
+++ Assembly-csharp.dll/_2DxFX.cs
@@ -0,0 +1,9 @@
+using System;
+using UnityEngine;
+
+// Token: 0x02000058 RID: 88
+public class _2DxFX : MonoBehaviour
+{
+	// Token: 0x040002C0 RID: 704
+	public static bool ActiveShadow = true;
+}
